008d3fb92762fcaab09187c3395c86b1b1436f81
Dreagonmon <531486058@qq.com>
2020-05-23 01:49:06 +0800

    使用mpy内置btree，实现更快的utf-8转gb2312的方法

---
+++ text_utils/u8_gb2312.py
@@ -0,0 +1,55 @@
+'''
+基于btree的unicode to gb2312编码转换工具
+import u8_gb2312
+u8_gb2312.init("unicode2gb2312.btree")
+u8_gb2312.convert_u8_gb2312("你好")
+u8_gb2312.convert_u8_gb2312("你好".encode())
+'''
+try:
+    from . import coding
+except:
+    import coding
+import btree
+
+__dbf = None
+__db = None
+def init(btree_file):
+    global __dbf, __db
+    if __dbf == None or __db == None:
+        __dbf = open(btree_file,"rb")
+        __db = btree.open(__dbf)
+
+def deinit():
+    global __dbf, __db
+    __db.close()
+    __dbf.close()
+
+def __convert(unic):
+    global __db
+    try:
+        key = unic.to_bytes(2,'big')
+        return __db[key]
+    except:
+        return False
+
+def convert_u8_gb2312(byts):
+    if type(byts) == str:
+        byts = byts.encode()
+    index = 0
+    res = bytearray()
+    while index < len(byts):
+        fb = byts[index]
+        l = coding.UTF_8.u8len(fb)
+        if l == 1:
+            index += 1
+            res.extend(coding.GB2312.ascii2gb2312(fb))
+            continue
+        unic = coding.UTF_8.u82unicode(byts[index:index+l])
+        bs = __convert(unic)
+        if not bs:
+            index += l
+            res.extend(coding.GB2312.ascii2gb2312(32))
+            continue
+        res.extend(bs)
+        index += l
+    return res
