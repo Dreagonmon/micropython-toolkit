333a5c462bb50cd02bcc2e0ea170c9b2c42a3837
Dreagonmon <531486058@qq.com>
2020-11-24 09:46:13 +0800

    add umid player, and a example using PWM to play mid music

---
+++ sound_utils/bee_singer.py
@@ -0,0 +1,73 @@
+'''
+    ESP32 Micropython mid micro player, using a bee
+'''
+import umid
+
+# index 0 is note C(-1)
+note_freq = [16, 17, 18, 19, 21, 22, 23, 24, 26, 28, 29, 31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 92, 98, 104, 110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1046, 1109, 1175, 1244, 1318, 1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2218, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520, 3729, 3951, 4186, 4435, 4699, 4978, 5274, 5588, 5920, 6272, 6645, 7040, 7459, 7902, 8372, 8870, 9397, 9956, 10548, 11175, 11840, 12544, 13290, 14080, 14917, 15804]
+volume_duty = [1023, 1022, 1021, 1020, 1017, 1015, 1013, 1010, 1000, 512]
+
+from machine import Pin, PWM, Timer
+from micropython import schedule
+# tim0 = Timer(0)
+# tim0.init(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(0))
+SIGNAL_DEFAULT = 0
+SIGNAL_STOP = -1
+class BeeSinger():
+    def __init__(self, bee_io, timer_id, mid_path):
+        self.__pwm = PWM(Pin(bee_io), freq=100000, duty=volume_duty[0])
+        with open(mid_path, "rb") as f:
+            self.__player = umid.MIDIPlayer(f)
+        self.__timer = Timer(timer_id)
+        self.__volume = volume_duty[0]
+        self.__loop = False
+        self.__track = 0
+    
+    def turn_on(self):
+        self.__pwm.init(freq=100000, duty=volume_duty[0])
+
+    def turn_off(self):
+        ''' not just stop, but turn off completely'''
+        self.__pwm.deinit()
+    
+    def _timer_callback(self, _):
+        schedule(self.play_next_note, None)
+
+    def start(self):
+        self.__player.reset(self.__track)
+        self.__timer.init(mode=Timer.ONE_SHOT, period=0, callback=self._timer_callback)
+
+    def stop(self):
+        self.__timer.deinit()
+        self.__pwm.freq(100000)
+        self.__pwm.duty(volume_duty[0])
+
+    def play_next_note(self, _):
+        during, event = self.__player.next_event(self.__track)
+        if event.type == umid.TRACK_EVENT_TYPE_META_RESET and event.extra == umid.META_TYPE_END_OF_TRACK:
+            self.start() if self.__loop else self.stop()
+            return
+        if event.type == umid.TRACK_EVENT_TYPE_NOTE_OFF:
+            note = event.data[0]
+            note = 0 if note < 0 else note
+            note = len(note_freq) - 1 if note >= len(note_freq) else note
+            self.__pwm.freq(note_freq[note])
+            self.__pwm.duty(volume_duty[1])
+        elif event.type == umid.TRACK_EVENT_TYPE_NOTE_ON:
+            note = event.data[0]
+            note = 0 if note < 0 else note
+            note = len(note_freq) - 1 if note >= len(note_freq) else note
+            self.__pwm.freq(note_freq[note])
+            self.__pwm.duty(self.__volume)
+        during = during // 1000 # us -> ms
+        self.__timer.init(mode=Timer.ONE_SHOT, period=during, callback=self._timer_callback)
+
+    def set_volume(self, select):
+        ''' set volume 0~9 '''
+        self.__volume = volume_duty[select]
+
+    def set_loop(self, is_loop):
+        self.__loop = is_loop
+
+    def select_track(self, track_id):
+        self.__track = track_id
---
+++ sound_utils/umid.py
@@ -0,0 +1,220 @@
+
+TRACK_TYPE_SINGLE = 0x00
+TRACK_TYPE_MULTI_SYNC = 0x01
+TRACK_TYPE_MULTI_NOSYNC = 0x02
+DIVISION_TYPE_TICKS = 0X00 # default 4/4 note length, 120bpm, 6beat
+DIVISION_TYPE_SMTPE = 0X01
+TRACK_EVENT_TYPE_MASK1 = 0b1111_0000
+TRACK_EVENT_TYPE_NOTE_OFF = 0b1000_0000
+TRACK_EVENT_TYPE_NOTE_ON = 0b1001_0000
+TRACK_EVENT_TYPE_POLYPHONIC_KEY_PRESSURE  = 0b1010_0000
+TRACK_EVENT_TYPE_CONTROL_CHANGE = 0b1011_0000
+TRACK_EVENT_TYPE_PROGRAM_CHANGE = 0b1100_0000
+TRACK_EVENT_TYPE_CHANNEL_PRESSURE = 0b1101_0000
+TRACK_EVENT_TYPE_PITCH_WHEEL_CHANGE = 0b1110_0000
+TRACK_EVENT_TYPE_SYSTEM = 0b1111_0000
+TRACK_EVENT_TYPE_MASK2 = 0b0000_1111
+TRACK_EVENT_TYPE_SYSTEM_EXCLUSIVE = 0b1111_0000
+TRACK_EVENT_TYPE_SONG_POSITION_POINTER = 0b1111_0010
+TRACK_EVENT_TYPE_SONG_SELECT = 0b1111_0011
+TRACK_EVENT_TYPE_TUNE_REQUEST = 0b1111_0110
+TRACK_EVENT_TYPE_END_OF_EXCLUSIVE = 0b1111_0111 # also ESCAPE
+TRACK_EVENT_TYPE_TIMING_CLOCK = 0b1111_1000
+TRACK_EVENT_TYPE_START = 0b1111_1010
+TRACK_EVENT_TYPE_CONTINUE = 0b1111_1011
+TRACK_EVENT_TYPE_STOP = 0b1111_1100
+TRACK_EVENT_TYPE_ACTIVE_SENSING = 0b1111_1110
+TRACK_EVENT_TYPE_META_RESET = 0b1111_1111 # meta-data
+TRACK_EVENT_LENGTH = {
+    TRACK_EVENT_TYPE_NOTE_OFF: 2,
+    TRACK_EVENT_TYPE_NOTE_ON: 2,
+    TRACK_EVENT_TYPE_POLYPHONIC_KEY_PRESSURE: 2,
+    TRACK_EVENT_TYPE_CONTROL_CHANGE: 2,
+    TRACK_EVENT_TYPE_PROGRAM_CHANGE: 1,
+    TRACK_EVENT_TYPE_CHANNEL_PRESSURE: 1,
+    TRACK_EVENT_TYPE_PITCH_WHEEL_CHANGE: 2,
+    # TRACK_EVENT_TYPE_SYSTEM: see below
+}
+TRACK_SYSTEM_EVENT_LENGTH = {
+    TRACK_EVENT_TYPE_SYSTEM_EXCLUSIVE: -1,
+    TRACK_EVENT_TYPE_SONG_POSITION_POINTER: 2,
+    TRACK_EVENT_TYPE_SONG_SELECT: 1,
+    TRACK_EVENT_TYPE_END_OF_EXCLUSIVE: -1,
+    TRACK_EVENT_TYPE_META_RESET: -1,
+    # else 0
+}
+META_TYPE_END_OF_TRACK = 0x2F
+META_TYPE_SET_TEMPO = 0x51
+
+class MIDITrackEvent():
+    def __init__(self, delta, event_type, event_data, event_extra=None):
+        self.delta = delta
+        self.type = event_type
+        self.data = event_data
+        self.extra = event_extra
+
+    def __repr__(self):
+        return '<MIDITrackEvent ticks="{:d}" type="{:X}" data="{}"/>'.format(self.delta, self.type, self.data)
+
+def _read_until(stream, ending, read_block_size = 1):
+    data = b''
+    block = stream.read(read_block_size)
+    readed = read_block_size
+    while len(block) > 0:
+        data += block
+        if data.endswith(ending):
+            return data
+        block = stream.read(read_block_size)
+        readed += read_block_size
+    return readed, data
+
+def _read_dyn_uint(stream):
+    num = 0
+    byt = stream.read(1)[0]
+    readed = 1
+    while byt & 0x80:
+        num |= (byt & 0x7F)
+        num <<= 7
+        byt = stream.read(1)[0]
+        readed += 1
+    num |= byt
+    return readed, num
+
+def _get_midi_track_event_length(event_type):
+    if (event_type & TRACK_EVENT_TYPE_MASK1) in TRACK_EVENT_LENGTH:
+        return TRACK_EVENT_LENGTH[event_type & TRACK_EVENT_TYPE_MASK1]
+    if event_type in TRACK_SYSTEM_EVENT_LENGTH:
+        return TRACK_SYSTEM_EVENT_LENGTH[event_type]
+    return 0
+
+def _read_midi_track_event(stream):
+    ''' return (readed_size, MIDITrackEvent) '''
+    readed, delta = _read_dyn_uint(stream)
+    event_type = stream.read(1)[0]
+    readed += 1
+    event_length = _get_midi_track_event_length(event_type)
+    if event_length >= 0:
+        return readed + event_length, MIDITrackEvent(delta, event_type, stream.read(event_length))
+    elif event_type == TRACK_EVENT_TYPE_SYSTEM_EXCLUSIVE:
+        size, data = _read_until(stream, bytes([TRACK_EVENT_TYPE_END_OF_EXCLUSIVE]), 1)
+        return readed + size, MIDITrackEvent(delta, event_type, data[:-1])
+    elif event_type == TRACK_EVENT_TYPE_END_OF_EXCLUSIVE:
+        readed2, size = _read_dyn_uint(stream)
+        readed += readed2
+        return readed + size, MIDITrackEvent(delta, event_type, stream.read(size))
+    else: # meta
+        meta_type = stream.read(1)[0]
+        readed += 1
+        readed2, size = _read_dyn_uint(stream)
+        readed += readed2
+        return readed + size,MIDITrackEvent(delta, event_type, stream.read(size), meta_type)
+
+def _read_midi_header(stream, check_id=True):
+    ''' return (readed_size, track_type, track_count, division) '''
+    if check_id:
+        assert stream.read(4) == b'MThd'
+    assert stream.read(4) == b'\x00\x00\x00\x06'
+    track_type = int.from_bytes(stream.read(2), 'big')
+    track_count = int.from_bytes(stream.read(2), 'big')
+    division = int.from_bytes(stream.read(2), 'big')
+    # division_type = note_ticks >> 15 # 0:每个四分音符的ticks 1:每秒中SMTPE帧的数量
+    # division = note_ticks & 0x7FFF
+    return 14 if check_id else 10, track_type, track_count, division
+
+def _read_midi_track(stream, check_id=True):
+    ''' return (readed_size, track_event_list:[MIDITrackEvent]) '''
+    readed = 0
+    if check_id:
+        assert stream.read(4) == b'MTrk'
+        readed += 4
+    track_data_size = int.from_bytes(stream.read(4), 'big')
+    content_count = 0
+    event_list = []
+    readed2, event = _read_midi_track_event(stream)
+    while True:
+        event_list.append(event)
+        readed += readed2
+        content_count += readed2
+        if content_count < track_data_size:
+            readed2, event = _read_midi_track_event(stream)
+        else:
+            break
+    return readed, MIDITrack(event_list)
+
+class MIDITrack():
+    def __init__(self, events):
+        self.events = events
+        self.meta = {
+            META_TYPE_SET_TEMPO: b'\x07\xA1\x20' # 500_000us
+        }
+        start_event_index = 0 # first no-meta event index
+        for event in events:
+            if event.type == TRACK_EVENT_TYPE_META_RESET:
+                self.meta[event.extra] = event.data
+                start_event_index += 1
+            else:
+                break
+        self.events = self.events[start_event_index:]
+        self.us_per_beat = int.from_bytes(self.meta[META_TYPE_SET_TEMPO], 'big')
+    
+    def __repr__(self):
+        return '<MIDITrack events="{}" us_per_beat="{}" />'.format(len(self.events), self.us_per_beat)
+
+class MIDIFile():
+    def __init__(self, stream):
+        self.__stream = stream
+        _, self.__track_type, self.__track_count, self.__division = _read_midi_header(stream)
+        self.tracks = []
+        for _ in range(self.__track_count):
+            _, track = _read_midi_track(stream)
+            self.tracks.append(track)
+    @property
+    def track_type(self):
+        return self.__track_type
+    @property
+    def track_count(self):
+        return self.__track_count
+    @property
+    def division(self):
+        return self.__division
+
+    def __repr__(self):
+        return '<MIDIFile tracks="{}" division="{}" />'.format(self.__track_count, self.__division)
+
+class MIDIPlayer():
+    def __init__(self, stream):
+        self.__midobj = MIDIFile(stream)
+        self.__pointer = [0] * self.__midobj.track_count
+        self.__ups = []
+        for track in self.__midobj.tracks:
+            self.__ups.append(track.us_per_beat)
+    
+    def next_event(self, track_id):
+        ''' return next_us, track_event '''
+        pos = self.__pointer[track_id]
+        event = self.__midobj.tracks[track_id].events[pos]
+        if event.type == TRACK_EVENT_TYPE_META_RESET and event.extra == META_TYPE_SET_TEMPO:
+            self.__ups[track_id] = int.from_bytes(event.data, 'big')
+        if event.type == TRACK_EVENT_TYPE_META_RESET and event.extra == META_TYPE_END_OF_TRACK:
+            return 0, event
+        pos += 1
+        self.__pointer[track_id] = pos
+        if event.type == TRACK_EVENT_TYPE_NOTE_OFF or event.type == TRACK_EVENT_TYPE_NOTE_ON:
+            next_event = self.__midobj.tracks[track_id].events[pos]
+            next_us = next_event.delta * self.__ups[track_id] // self.__midobj.division
+            return next_us, event
+        else:
+            return self.next_event(track_id) # ignore other event
+
+    def reset(self, track_id):
+        self.__pointer[track_id] = 0
+        self.__ups[track_id] = self.__midobj.tracks[track_id].us_per_beat
+
+
+if __name__ == "__main__":
+    f = open("dm2.mid", "rb")
+    midf = MIDIFile(f)
+    f.close()
+    print(midf)
+    for track in midf.tracks:
+        print(track)
