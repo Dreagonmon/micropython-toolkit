396c16036f2e7ed9f791531ed66f60691eeef596
Dreagonmon <531486058@qq.com>
2021-07-25 16:42:17 +0800

    nfs

--- tool_utils/remote_block_device/nblockdev.py
+++ tool_utils/remote_block_device/nblockdev.py
@@ -1,94 +1,158 @@
 '''
 dev = NetworkBlockDevice("192.168.31.31", 12588)
-os.VfsLfs2.mkfs(dev)
-os.mount(dev, '/c')
+try:
+    fs = os.VfsLfs2(dev)
+except:
+    fs = os.VfsLfs2.mkfs(dev)
+os.mount(fs, '/mnt')
 '''
 from micropython import const
-import socket, select
-TIMEOUT = const(5000) # ms
-RETRY = const(3)
-CODE_SUCCESS = const(0XFF)
-CMD_WRITE = const(0x00)
-CMD_READ = const(0x01)
-CMD_GET_INFO = const(0x02)
-CMD_PING = const(0xFF)
-
-pid = 0
+import usocket
 
 class RemoteResourceError(Exception):
     pass
 
-class NetworkBlockDevice:
-    def __init__(self, host, port=12588):
-        udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        poller = select.poll()
-        poller.register(udp, select.POLLIN)
-        address = socket.getaddrinfo(host, port)[0][-1]
-        udp.sendto(bytearray([CMD_GET_INFO]), address)
-        res = poller.poll(TIMEOUT)
-        if len(res) > 0:
-            data = udp.recv(512)
-        else:
-            raise RemoteResourceError()
-        block_size = int.from_bytes(data[0:4], 'big')
-        block_count = int.from_bytes(data[4:8], 'big')
-        assert block_size == 256
+# UDP Protocol
+CODE_SUCCESS = const(0XFF)
+CMD_WRITE = const(0x00)
+CMD_READ = const(0x01)
+CMD_GET_INFO = const(0x02)
+CMD_GET_SERVER_INFO = const(0x03)
+CMD_PING = const(0xFF)
+class ProtocolUDP:
+    def __init__(self, host, port=12588, retry=3, timeout_ms=500):
+        udp = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
+        udp.settimeout(timeout_ms/1000)
+        address = usocket.getaddrinfo(host, port)[0][-1]
+        # get basic server info
+        udp.sendto(bytearray([CMD_GET_SERVER_INFO]), address)
+        data = udp.recv(8)
+        self.param_size = int.from_bytes(data[0:4], 'big')
+        self.custom_param_offset = int.from_bytes(data[4:8], 'big')
+        self.custom_param_size = self.param_size - self.custom_param_offset
+        # get remote block device info
+        req = bytearray(self.param_size)
+        req[0] = CMD_GET_INFO
+        # todo: inject custom param
+        req[self.custom_param_offset:self.param_size] = bytearray(self.custom_param_size)
+        udp.sendto(req, address)
+        data = udp.recv(self.param_size)
+        self.block_size = int.from_bytes(data[0:4], 'big')
+        self.block_count = int.from_bytes(data[4:8], 'big')
+        self.pid = 0 # request id
         self.udp = udp
-        self.poller = poller
         self.address = address
-        self.block_size = block_size # block_size
-        self.block_count = block_count # block_count
-        # print("bdev:", block_size, block_count)
+        self.retry = retry
+        self.packet_size = self.param_size + self.block_size
+    
+    def get_info(self):
+        return self.block_size, self.block_count
 
-    def __readblock(self, block_num, debug=False):
-        global pid
-        if debug:
-            print("recv_from:", block_num)
-        retry = RETRY
+    def read_block(self, block_num):
+        print("read:", block_num)
+        retry = self.retry
         while retry > 0:
             try:
-                id = pid % 0xFF
-                pid += 1
-                req = bytearray(512)
+                id = self.pid % 0xFF
+                self.pid += 1
+                req = bytearray(self.packet_size)
                 req[0] = CMD_READ
                 req[1] = id
                 req[2:6] = int.to_bytes(block_num, 4, 'big')
                 self.udp.sendto(req, self.address)
-                res = self.poller.poll(TIMEOUT)
-                if len(res) > 0:
-                    data = self.udp.recv(512)
-                    if data[0] == CODE_SUCCESS and data[1] == id:
-                        if debug:
-                            print(data[256:512])
-                        return data[256:512]
+                data = self.udp.recv(self.packet_size)
+                if data[0] == CODE_SUCCESS and data[1] == id:
+                    return data[self.param_size:self.packet_size]
+            except: pass
+            retry -= 1
+        raise RemoteResourceError()
+    
+    def write_block(self, block_num, data):
+        print("write:", block_num)
+        retry = self.retry
+        while retry > 0:
+            try:
+                id = self.pid % 0xFF
+                self.pid += 1
+                req = bytearray(self.packet_size)
+                req[0] = CMD_WRITE
+                req[1] = id
+                req[2:6] = int.to_bytes(block_num, 4, 'big')
+                req[self.param_size:self.packet_size] = data[:self.block_size]
+                self.udp.sendto(req, self.address)
+                data = self.udp.recv(self.packet_size)
+                if data[0] == CODE_SUCCESS and data[1] == id:
+                    return
             except: pass
             retry -= 1
         raise RemoteResourceError()
 
-    def __writeblock(self, block_num, data, debug=False):
-        global pid
-        if debug:
-            print("send_to:", block_num)
-            print(data)
-        retry = RETRY
-        while retry > 0:
-            try:
-                id = id = pid % 0xFF
-                pid += 1
-                req = bytearray(512)
-                req[0] = CMD_WRITE
-                req[1] = id
-                req[2:6] = int.to_bytes(block_num, 4, 'big')
-                req[256:512] = data[:256]
-                self.udp.sendto(req, self.address)
-                res = self.poller.poll(TIMEOUT)
-                if len(res) > 0:
-                    data = self.udp.recv(512)
-                    if data[0] == CODE_SUCCESS and data[1] == id:
-                        return
-            except: pass
-            retry -= 1
-        raise RemoteResourceError()
+
+# Cache Protocol
+class ProtocolCache():
+    def __init__(self, protocol_obj, cache_blocks=5):
+        self.__protocol = protocol_obj
+        self.get_info = protocol_obj.get_info
+        protocol_info = protocol_obj.get_info()
+        self.block_size = protocol_info[0] # block_size
+        self.block_count = protocol_info[1] # block_count
+        self.__cache_list = []
+        for i in range(cache_blocks):
+            # [block_number, data, visited]
+            self.__cache_list.append([-1, bytearray(self.block_size), 0])
+            # 按照最不常用原则淘汰缓存。缓存命中时visited+=1，其余缓存不变。缓存未命中时所有visited-1。
+            # visited=<0时可以替换，替换时优先替换visited最小的缓存
+
+    def __query_cache(self, block_num):
+        for cache in self.__cache_list:
+            if cache[0] == block_num:
+                cache[2] += 1
+                return cache[1]
+        # 缓存未命中
+        for cache in self.__cache_list:
+            cache[2] -= 1
+        return None
+
+    def __update_cache(self, block_num, data):
+        target_cache = None
+        # 找到最不常用的缓存
+        for cache in self.__cache_list:
+            if target_cache == None or cache[2] < target_cache[2]:
+                target_cache = cache
+            if cache[0] == block_num:
+                # 缓存存在，直接更新
+                cache[1][:] = data[:]
+                return
+        # 改写缓存
+        if target_cache != None and target_cache[2] <= 0:
+            target_cache[0] = block_num
+            target_cache[1][:] = data[:]
+            target_cache[2] = 1
+
+    def read_block(self, block_num):
+        cache = self.__query_cache(block_num)
+        if cache != None:
+            print("cache hit:", block_num)
+            return cache
+        cache = self.__protocol.read_block(block_num)
+        self.__update_cache(block_num, cache)
+        print("cache update:", block_num)
+        return cache
+
+    def write_block(self, block_num, data):
+        _ = self.__query_cache(block_num)
+        self.__protocol.write_block(block_num, data)
+        self.__update_cache(block_num, data)
+        print("cache update:", block_num)
+
+# Network Block Device
+class NetworkBlockDevice:
+    def __init__(self, protocol_obj):
+        self.protocol = protocol_obj
+        protocol_info = protocol_obj.get_info()
+        self.block_size = protocol_info[0] # block_size
+        self.block_count = protocol_info[1] # block_count
+        # print("bdev:", block_size, block_count)
 
     def readblocks(self, block_num, buf, offset=0):
         # print(">read:",len(buf),"block:",block_num,"offset:",offset)
@@ -99,17 +163,17 @@ class NetworkBlockDevice:
             b_end = offset + size
             b_end = 256 if b_end > 256 else b_end
             h_size = b_end - offset
-            b_data = self.__readblock(blkn)
+            b_data = self.protocol.read_block(blkn)
             buf[0:h_size] = b_data[offset:b_end]
             index += h_size
             blkn += 1
         while size - index >= 256:
-            b_data = self.__readblock(blkn)
+            b_data = self.protocol.read_block(blkn)
             buf[index: index+256] = b_data[:256]
             index += 256
             blkn += 1
         if size != index:
-            b_data = self.__readblock(blkn)
+            b_data = self.protocol.read_block(blkn)
             buf[index:size] = b_data[0:size - index]
         # print(buf[:])
         # print("========")
@@ -124,20 +188,20 @@ class NetworkBlockDevice:
             b_end = offset + size
             b_end = 256 if b_end > 256 else b_end
             h_size = b_end - offset
-            b_data = bytearray(self.__readblock(blkn))
+            b_data = bytearray(self.protocol.read_block(blkn))
             b_data[offset:b_end] = buf[0:h_size]
-            self.__writeblock(blkn, b_data)
+            self.protocol.write_block(blkn, b_data)
             index += h_size
             blkn += 1
         while size - index >= 256:
             b_data = buf[index: index+256]
-            self.__writeblock(blkn, b_data)
+            self.protocol.write_block(blkn, b_data)
             index += 256
             blkn += 1
         if size != index:
-            b_data = bytearray(self.__readblock(blkn))
+            b_data = bytearray(self.protocol.read_block(blkn))
             b_data[0:size - index] = buf[index:size]
-            self.__writeblock(blkn, b_data)
+            self.protocol.write_block(blkn, b_data)
         # print("========")
 
     def ioctl(self, op, arg):
@@ -148,7 +212,7 @@ class NetworkBlockDevice:
         if op == 6: # block erase, arg is block_num
             try:
                 # print(">erase:",arg)
-                self.__writeblock(arg, bytearray(256))
+                self.protocol.write_block(arg, bytearray(self.block_size))
                 # print("========")
                 return 0
             except RemoteResourceError:
--- tool_utils/remote_block_device/nlfs_server.py
+++ tool_utils/remote_block_device/nlfs_server.py
@@ -1,85 +1,187 @@
 import io
-import mmap
-from littlefs import LittleFS
-from littlefs.context import UserContext
 import socket
-BLOCK_SIZE = 256
+
+class SimpleCFG():
+    def __init__(self, block_size, block_count):
+        self.__block_size = block_size
+        self.__block_count = block_count
+    
+    @property
+    def block_size(self):
+        return self.__block_size
+    
+    @property
+    def block_count(self):
+        return self.__block_count
+    
+    @property
+    def buffsize(self):
+        return self.__block_size * self.__block_count
+
+class FileBlockDevice():
+    def __init__(self, file_path, block_size, block_count):
+        self.__cfg = SimpleCFG(block_size, block_count)
+        self.__path = file_path
+        self.__file = None
+    
+    @property
+    def config(self):
+        return self.__cfg
+
+    def __enter__(self):
+        self.open()
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return self.close()
+
+    def open(self):
+        try:
+            self.__file = open(self.__path, "rb+")
+        except:
+            self.__file = open(self.__path, "wb+")
+            self.__file.write(bytearray(self.__cfg.buffsize))
+            self.__file.flush()
+        return self
+
+    def close(self):
+        if self.__file == None:
+            return
+        self.__file.close()
+        self.__file = None
+    
+    def get_info(self):
+        return self.__cfg.block_size, self.__cfg.block_count
+    
+    def read_block(self, block_number):
+        offset = block_number * self.__cfg.block_size
+        # print(">read:", block_num, "offset:", offset)
+        self.__file.seek(offset, io.SEEK_SET)
+        return self.__file.read(self.__cfg.block_size)
+    
+    def write_block(self, block_number, data):
+        offset = block_number * self.__cfg.block_size
+        self.__file.seek(offset, io.SEEK_SET)
+        self.__file.write(data[:self.__cfg.block_size])
+        self.__file.flush()
+
 CODE_SUCCESS = 0XFF
 CMD_WRITE = 0x00
 CMD_READ = 0x01
 CMD_GET_INFO = 0x02
+CMD_GET_SERVER_INFO = 0x03
 CMD_PING = 0xFF
+class ServerUDP():
+    def __init__(self, host="0.0.0.0", port=12588, custom_param_size=0):
+        udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp.settimeout(0.5)
+        udp.bind((host, port))
+        self.udp = udp
+        self.param_size = self.MINIMAL_PARAM_SIZE + custom_param_size
+        self.custom_param_offset = self.MINIMAL_PARAM_SIZE
+        self.custom_param_size = custom_param_size
+    
+    @property
+    def MINIMAL_PARAM_SIZE(_):
+        return 32
 
-class SimpleCFG():
-    block_size = 0
-    block_count = 0
+    def do_udp_request(self, block_device: FileBlockDevice, data:bytes, client):
+        # block_count = block_device.config.block_count
+        # no block_device require:
+        cmd = data[0]
+        if cmd == CMD_PING:
+            self.udp.sendto(bytearray(1), client)
+            return
+        elif cmd == CMD_GET_SERVER_INFO: # data size 1
+            resp = bytearray(8) # fix size 8
+            resp[0:4] = int.to_bytes(self.param_size, 4, 'big') # param size
+            resp[4:8] = int.to_bytes(self.custom_param_offset, 4, 'big') # custom param offset
+            self.udp.sendto(resp, client)
+            return
+        # block_device required:
+        block_size = block_device.config.block_size
+        param_size = self.param_size
+        packet_size = block_size + param_size
+        packet_data = data[param_size:packet_size]
+        if cmd == CMD_GET_INFO: # data size param_size
+            bs, bc = block_device.get_info()
+            resp = bytearray(param_size) # fix size param_size
+            resp[0:4] = int.to_bytes(bs, 4, 'big') # block size
+            resp[4:8] = int.to_bytes(bc, 4, 'big') # block count
+            self.udp.sendto(resp, client)
+        elif cmd == CMD_READ: # data size packet_size
+            block_num = int.from_bytes(data[2:6], 'big')
+            resp_data = block_device.read_block(block_num)
+            resp = bytearray(packet_size)
+            resp[0] = CODE_SUCCESS
+            resp[1] = data[1]
+            resp[param_size:packet_size] = resp_data
+            self.udp.sendto(resp, client)
+        elif cmd == CMD_WRITE: # data size packet_size
+            block_num = int.from_bytes(data[2:6], 'big')
+            block_device.write_block(block_num, packet_data[:block_size])
+            resp = bytearray(packet_size)
+            resp[0] = CODE_SUCCESS
+            resp[1] = data[1]
+            self.udp.sendto(resp, client)
+            # print("========")
+    
+    def run(self, custom_callback):
+        try:
+            while True:
+                try:
+                    data, client = self.udp.recvfrom(65536)
+                    if data[0] == CMD_GET_SERVER_INFO:
+                        self.do_udp_request(None, data, client)
+                    else:
+                        block_data_size = len(data) - self.custom_param_size - self.MINIMAL_PARAM_SIZE
+                        data, file_block_device = custom_callback(self, data, self.MINIMAL_PARAM_SIZE, self.custom_param_size, block_data_size)
+                        with file_block_device:
+                            self.do_udp_request(file_block_device, data, client)
+                except socket.timeout:
+                    continue
+                except KeyboardInterrupt as e:
+                    raise e
+                except:
+                    import traceback
+                    traceback.print_exc()
+        except KeyboardInterrupt:
+            pass
 
-class BlockDeviceFile(UserContext):
-    def __init__(self, file, buffsize: int) -> None:
-        self.buffer = mmap.mmap(file.fileno(), buffsize)
-
-if __name__ == "__main__":
-    # block_count = 524288 # 128MB
-    block_count = 4096 # 1MB
+def __main():
+    from littlefs import LittleFS
+    from littlefs.context import UserContext
+    import mmap
     port = 12588
-    try:
-        fs_file = open("lfs.img", "rb+")
-        print("fsf loaded.")
-    except:
-        fs_file = open("lfs.img", "wb+")
-        fs_file.write(bytearray(BLOCK_SIZE*block_count))
-        fs_file.flush()
-        print("fsf inited.")
+    block_size = 1024
+    block_count = 4096 # 1MB
+    file_block_device = FileBlockDevice("lfs.img", block_size, block_count)
+    def cus_callback(server, data, m_param_size, c_param_size, block_data_size):
+        if block_data_size < 0:
+            return data, None
+        param = data[0:m_param_size]
+        custom_param = data[m_param_size:m_param_size+c_param_size]
+        block_data = data[m_param_size+c_param_size:m_param_size+c_param_size+block_data_size]
+        return param + custom_param + block_data, file_block_device
     # listen
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp.settimeout(0.5)
-    udp.bind(('0.0.0.0', port))
-    try:
-        while True:
-            try:
-                data, client = udp.recvfrom(512)
-            except socket.timeout:
-                continue
-            cmd = data[0]
-            packet_data = data[512-BLOCK_SIZE:512]
-            if cmd == CMD_PING:
-                udp.sendto(bytearray(1), client)
-            elif cmd == CMD_GET_INFO:
-                resp = b''
-                resp += int.to_bytes(BLOCK_SIZE, 4, 'big') # block size
-                resp += int.to_bytes(block_count, 4, 'big') # block count
-                udp.sendto(resp, client)
-            elif cmd == CMD_READ:
-                block_num = int.from_bytes(data[2:6], 'big')
-                offset = block_num * BLOCK_SIZE
-                # print(">read:", block_num, "offset:", offset)
-                fs_file.seek(offset, io.SEEK_SET)
-                resp_data = fs_file.read(BLOCK_SIZE)
-                resp = bytearray(512)
-                resp[0] = CODE_SUCCESS
-                resp[1] = data[1]
-                resp[512-BLOCK_SIZE:512] = resp_data
-                udp.sendto(resp, client)
-            elif cmd == CMD_WRITE:
-                block_num = int.from_bytes(data[2:6], 'big')
-                offset = block_num * BLOCK_SIZE
-                # print(">write:", block_num, "offset:", offset)
-                # print(packet_data[:BLOCK_SIZE])
-                fs_file.seek(offset, io.SEEK_SET)
-                fs_file.write(packet_data[:BLOCK_SIZE])
-                resp = bytearray(512)
-                resp[0] = CODE_SUCCESS
-                resp[1] = data[1]
-                udp.sendto(resp, client)
-                # print("========")
-    except KeyboardInterrupt:
-        pass
+    server = ServerUDP(port=12588, custom_param_size=16)
+    file_block_device.open()
+    server.run(cus_callback)
+    file_block_device.close()
     # output content
-    fs_file.flush()
-    context = BlockDeviceFile(fs_file, BLOCK_SIZE*block_count)
-    fs = LittleFS(context, block_size=BLOCK_SIZE, block_count=block_count)
+    class FileContext(UserContext):
+        def __init__(self, file, buffsize):
+            self.buffer = mmap.mmap(file.fileno(), buffsize)
+    cfg = file_block_device.config
+    context_file = open("lfs.img", "r+")
+    context = FileContext(context_file, cfg.buffsize)
+    fs = LittleFS(context, block_size=cfg.block_size, block_count=cfg.block_count)
     print(fs.stat("/"))
     print(fs.listdir("/"))
-    print(context.buffer[:256])
-    context.buffer.close()
-    fs_file.close()
+    file_size = fs.stat("README.md").size
+    f = fs.open("README.md", "r")
+    print(f.read(file_size).decode("utf8"))
+    f.close()
+    context_file.close()
+
+if __name__ == "__main__":
+    __main()
